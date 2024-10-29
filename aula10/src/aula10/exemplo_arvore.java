0a2b11abe875637b27d850b70fed1e5aeb224d23
KauePiotto <kauepiotto@gmail.com>
2024-10-29 09:41:08 -0300

    aula-arvore

---
+++ aula10/src/aula10/exemplo_arvore.java
@@ -0,0 +1,14 @@
+package aula10;
+
+import java.util.Random; //Usa a biblioteca de randomização
+
+public class exemplo_arvore {
+	public static void main(String[] args) {
+		Random rand = new Random(); // Inicializa a randomizzação
+		Arvore arvore = new Arvore(); // Cria uma árvore binária
+
+		for (int i = 0; i < 10; i++) { //Fazer 10 vezes
+			arvore.insert(rand.nextInt(100), arvore.root);//Insere um elemento aleatorio de 0 até 99
+		}
+	}
+}
